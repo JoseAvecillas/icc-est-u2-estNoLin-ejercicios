6f23a9ae752daf89427f324d34534cbf9a758364
JoseAvecillas <joseandres069087@gmail.com>
2025-07-06 12:25:25 -0500

    Octava prueba de correccion de errores, llegue a una conclusion, no se que metodo puede estar afectando para que me de un error, las clases imprimen bien

--- src/main/App.java
+++ src/main/App.java
@@ -9,11 +9,11 @@ import main.Ejercicio_04_depth.Depth;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // // NO MODIFICAR ESTE CÓDIGO
-        // var estudiante = StudentValidator.validarDatos();
-        // System.out.println("👤 Estudiante: " + estudiante.nombre());
-        // System.out.println("✉️  Correo: " + estudiante.correo());
-        // // NO MODIFICAR HASTA AQUÍ
+        // NO MODIFICAR ESTE CÓDIGO
+        var estudiante = StudentValidator.validarDatos();
+        System.out.println("👤 Estudiante: " + estudiante.nombre());
+        System.out.println("✉️  Correo: " + estudiante.correo());
+        // NO MODIFICAR HASTA AQUÍ
 
         System.out.println("===== Ejercicio 01: Insertar en BST =====");
         InsertBST.run();
